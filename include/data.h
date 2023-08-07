ebdfc86f19caeeb64f8ec3de01458bf732e0f380
jvap2 <jayvap6181@gmail.com>
2023-08-07 10:40:48 -0400

    make

--- include/data.h
+++ include/data.h
@@ -7,7 +7,7 @@ using namespace std;
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
-#include "GPUErrors.h"
+#include "../include/data.h"
 #define EDGES 5066842
 #define NODES 855802
 #define EDGE_PATH "../Data/webGoogle.csv"
