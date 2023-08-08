0908852b8950bf14c68fd92a11ea0a315c6d4973
jvap2 <jayvap6181@gmail.com>
2023-08-08 08:51:58 -0400

    python

--- Homogenous/data_synth/graph_partitioning.py
+++ Homogenous/data_synth/graph_partitioning.py
@@ -2,7 +2,7 @@ import cugraph
 import cudf
 
 num_clusters=10
-graph= cudf.read_csv("../Data/webGoogle.csv")
+graph= cudf.read_csv("../Data/webGoogle.csv", dtype=['int32', 'int32'])
 print(graph.head())
 g=cugraph.Graph()
 g.from_cudf_edgelist(graph, source='FromNodeId', destination='ToNodeId')
