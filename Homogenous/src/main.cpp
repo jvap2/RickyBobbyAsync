e85728bc8d786522654ba1747a2370d49535c72f
jvap2 <jayvap6181@gmail.com>
2023-08-09 14:04:40 -0400

    Sorting- Take 2

--- Homogenous/src/main.cpp
+++ Homogenous/src/main.cpp
@@ -7,7 +7,9 @@ int main()
 {  
     edge* edge_list;
     edge_list=(edge*)malloc(sizeof(edge)*EDGES);
+    cout<<"Starting the edge list function"<<endl;
     return_edge_list(EDGE_PATH,edge_list);
+    cout<<"Ending edge list function"<<endl;
     graph* G = create_graph(edge_list);
     cout<<"Starting Helper Function"<<endl;
     Org_Vertex_Helper(edge_list,EDGES);
