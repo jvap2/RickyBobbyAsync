73b73800447b030818512813217eba8204c3f254
jvap2 <jayvap6181@gmail.com>
2023-08-09 10:09:51 -0400

    Creating data structures for graph

--- Homogenous/src/main.cpp
+++ Homogenous/src/main.cpp
@@ -8,6 +8,10 @@ int main()
     edge* edge_list;
     edge_list=(edge*)malloc(sizeof(edge)*EDGES);
     return_edge_list(EDGE_PATH,edge_list);
+    for(int i = 0; i<32 ; i++){
+        cout<<edge_list[i].start<<endl;
+        cout<<edge_list[i].end<<endl;
+    }
     free(edge_list);
     return 0;
 }
