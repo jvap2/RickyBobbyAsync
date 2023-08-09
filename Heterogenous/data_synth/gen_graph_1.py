9f3a6b3f083a5b9d27332d6f705ecfc99a6d0886
jvap2 <jayvap6181@gmail.com>
2023-08-09 16:40:11 -0400

    Generate Graphs

---
+++ Heterogenous/data_synth/gen_graph_1.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import random
+import numpy as np
+
+Network=open("net.csv","w")
+
+
+line = ["#delimieter: ,\n","#columns: house,station\n","#types: UINT,UINT\n"]
+
+Network.write(line)
+
+num_p_system = int(sys.argv[2])
+num_houses = int(sys.argv[1])
+
+np.random.see(int(sys.argv[3]))
+
+for house in range(num_houses):
+    num_used = int( np.random.exponential(20.0) )
+    if num_used > 20: num_used = int( num_used / 10 )     # let's not generate too many edges
+
+    for i in range(0, num_used):
+        p_system = int( np.random.random() * num_p_system )
+        Network.write(str(house) + "," + str(p_system) + "\n")
