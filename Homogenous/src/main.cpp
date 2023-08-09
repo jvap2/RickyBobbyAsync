46242b7b3b434c51aab57199bb3dfaa128ad9d8d
jvap2 <jayvap6181@gmail.com>
2023-08-09 13:53:10 -0400

    Sorting- Take 2

--- Homogenous/src/main.cpp
+++ Homogenous/src/main.cpp
@@ -9,6 +9,7 @@ int main()
     edge_list=(edge*)malloc(sizeof(edge)*EDGES);
     return_edge_list(EDGE_PATH,edge_list);
     graph* G = create_graph(edge_list);
+    Org_Vertex_Helper(edge_list,EDGES);
     free(edge_list);
     return 0;
 }
