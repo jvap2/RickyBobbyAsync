ef26bba298777c668285fc45d9e0550ac4813766
jvap2 <jayvap6181@gmail.com>
2023-08-10 08:22:45 -0400

    Generate Graphs

--- Heterogenous/data_synth/gen_graph_1.py
+++ Heterogenous/data_synth/gen_graph_1.py
@@ -5,23 +5,24 @@ import numpy as np
 
 
 folder=os.getcwd()[:-10]
-Network=open(os.path.join(folder,"Data/net.csv"),"w")
-
-
-line = ["#delimieter: ,\n","#columns: house,station\n","#types: UINT,UINT\n", "house,station\n"]
-
-for l in line:
-    Network.write(l)
 
 num_p_system = int(sys.argv[2])
 num_houses = int(sys.argv[1])
 
 np.random.seed(int(sys.argv[3]))
+count=0
 
-for house in range(num_houses):
-    num_used = int( np.random.exponential(20.0) )
-    if num_used > 20: num_used = int( num_used / 10 )     # let's not generate too many edges
+line = ["#delimieter: ,\n","#columns: house,station\n","#types: UINT,UINT\n", "house,station\n"]
 
-    for i in range(0, num_used):
-        p_system = int( np.random.random() * num_p_system )
-        Network.write(str(house) + "," + str(p_system) + "\n")
+with open(os.path.join(folder,"Data/net.csv"),"w") as fp:
+    if count<len(line):
+        fp.write(line[count])
+        count+=1
+    else:
+        for house in range(num_houses):
+            num_used = int( np.random.exponential(20.0) )
+            if num_used > 20: num_used = int( num_used / 10 )     # let's not generate too many edges
+
+            for i in range(0, num_used):
+                p_system = int( np.random.random() * num_p_system )
+                fp.write(str(house) + "," + str(p_system) + "\n")
