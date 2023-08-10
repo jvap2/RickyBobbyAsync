efd4bfa7a70630abdf417ccb5e3f649dd7dd3a5d
jvap2 <jayvap6181@gmail.com>
2023-08-10 08:32:14 -0400

    Generate Graphs

--- Heterogenous/data_synth/gen_graph_1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-house=200
-p_station=50
-seed=4
-
-python3 gen_graph_1.py $house $p_station $seed 
---
+++ Heterogenous/data_synth/gen_het_graph_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+house=200
+p_station=50
+seed=4
+
+python3 gen_het_graph_1.py $house $p_station $seed 
