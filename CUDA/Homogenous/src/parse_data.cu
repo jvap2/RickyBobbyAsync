ac83a39e31bff8e64084cb2f77d1ecbc1af3fae6
jvap2 <jayvap6181@gmail.com>
2023-08-16 12:12:25 -0400

    Growing helper

--- CUDA/Homogenous/src/parse_data.cu
+++ CUDA/Homogenous/src/parse_data.cu
@@ -682,3 +682,58 @@ __global__ void First_Init(float* rand_frog, unsigned int* d_frog, unsigned int 
         atomicAdd(&d_frog[(int)rand_frog[idx]],1);
     }
 }
+
+
+
+
+
+
+
+
+__global__ void acc_accum(unsigned int* approx, unsigned int* pagerank, unsigned int* table, unsigned int k){
+    unsigned int idx=threadIdx.x + (blockIdx.x*blockDim.x);
+    unsigned int tid = threadIdx.x;
+    __shared__ unsigned int local_table[TPB];
+    if(idx<k){
+        if(approx[idx]==pagerank[idx]){
+            local_table[tid]=1;
+        }
+        else{
+            local_table[tid]=0;
+        }
+    }
+    for(unsigned int stride = 1; stride<blockDim.x;stride*=2){
+        __syncthreads();
+        unsigned int temp;
+        if(tid>=stride){
+            temp=local_table[tid]+local_table[tid-stride];
+        }
+        __syncthreads();
+        if(tid>=stride){
+            local_table[tid]=temp;
+        }
+    }
+    if(tid==(blockDim.x-1)){
+        local_table[tid]=table[idx];
+    }
+}
+
+__global__ float fin_acc(unsigned int* table, unsigned int k){
+    float acc;
+    unsigned int tid = threadIdx.x;
+    for(unsigned int stride = 1; stride<blockDim.x;stride*=2){
+        __syncthreads();
+        unsigned int temp;
+        if(tid>=stride){
+            temp=table[tid]+table[tid-stride];
+        }
+        __syncthreads();
+        if(tid>=stride){
+            table[tid]=temp;
+        }
+    }
+    if(tid==(blockDim.x-1)){
+        acc=float(table[tid])/float(k);
+    }
+    return acc;
+}
