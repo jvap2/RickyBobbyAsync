f1ca65146ff39d35ad6f6e7f052e603bb2bf2451
jvap2 <jayvap6181@gmail.com>
2023-08-19 09:27:02 -0400

    Yas

--- Py_Testing/Homogenous/test_frogwild.py
+++ Py_Testing/Homogenous/test_frogwild.py
@@ -17,12 +17,12 @@ def Get_Degree(edge_list, no_nodes):
         out_degree[e[1]]+=1
     return in_degree, out_degree
 
-def Degree_Cluster_Hash(in_degree, out_degree, no_nodes, clusters, edge_list):
+def Degree_Cluster_Hash(clusters, edge_list, in_d, out_d):
     cluster_assign={}
     for c in range(clusters):
         cluster_assign[c]=[]
     for e in edge_list:
-        cluster_assign[Random_Edge_Placement(e[0],e[1])].append({e[0]:e[1]})
+        cluster_assign[Random_Edge_Placement(max(in_d[e[0]]+out_d[e[0]],in_d[e[1]]+in_d[e[0]]))].append({e[0]:e[1]})
     return cluster_assign
 
 
@@ -37,10 +37,13 @@ df_graph_data=pl.read_csv(os.path.join(os.getcwd()[:-21],"Data/Homogenous/rand/r
 
 no_nodes, no_edges = df_graph_data.get_column("No. Nodes")[0], df_graph_data.get_column("No. Edges ")[0]
 
-to, frm = df_edge.get_column("to").to_numpy(), df_edge.get_column("from")
-num_out_neigh = frm.value_counts().sort(by='from').to_numpy()
+edge_list = [df_edge.get_column("to").to_numpy(), df_edge.get_column("from").to_numpy()]
 
+in_d, out_d =Get_Degree(edge_list, no_nodes)
 
+cluster_assign = Degree_Cluster_Hash(clusters, edge_list, in_d, out_d)
+
+print(cluster_assign)
 
 
 
