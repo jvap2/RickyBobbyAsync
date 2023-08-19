321abe249e8749455c979b9368c769220d6fe4d8
jvap2 <jayvap6181@gmail.com>
2023-08-19 09:17:29 -0400

    Yas

--- Py_Testing/Homogenous/test_frogwild.py
+++ Py_Testing/Homogenous/test_frogwild.py
@@ -8,26 +8,22 @@ global clusters
 clusters=32
 
 
-class Dir_Graph:
-    def __init__(self, no_edges):
-        self.no_nodes=0
-        self.no_edges=no_edges
-        self.src=[]
-        self.succ=[]
-        self.idx=[]
-        self.out_degree=[]
-    def gen_CSR(self, edge_list):
-        '''Here, we want to read the edge_list in and generate the CSR representation of the graph
-        The attributes of the class that will be worked on are the following:
-        (1) src: this points to the starting index of the successors to a node
-        (2) succ: this is the list of successors to a node
-        (3) idx: this is the index of the node
-        (4) out_degree: this is the out degree of the node
-        '''
-        idx,self.out_degree=np.unique(edge_list[:,0]) ##This provides a list of the unique outgoing values in the graph, and their respective counts
-        self.no_nodes=len(idx)
-        self.src=np.zeros(self.no_nodes+1)
 
+def Get_Degree(edge_list, no_nodes):
+    in_degree = np.zeroes(no_nodes)
+    out_degree = np.zeroes(no_nodes)
+    for e in edge_list:
+        in_degree[e[0]]+=1
+        out_degree[e[1]]+=1
+    return in_degree, out_degree
+
+def Degree_Cluster_Hash(in_degree, out_degree, no_nodes, clusters, edge_list):
+    cluster_assign={}
+    for c in range(clusters):
+        cluster_assign[c]=[]
+    for e in edge_list:
+        cluster_assign[Random_Edge_Placement(e[0],e[1])].append({e[0]:e[1]})
+    return cluster_assign
 
 
 def Random_Edge_Placement(i):
@@ -46,24 +42,7 @@ num_out_neigh = frm.value_counts().sort(by='from').to_numpy()
 
 
 
-g={}
-count=0
-for c,n in enumerate(num_out_neigh[:,0]):
-    g[n]=[]
-    for i in range(num_out_neigh[c,1]):
-        g[n].append(to[count])
-        count+=1
 
-
-cluster_assign={}
-for c in range(clusters):
-    cluster_assign[c]=[]
-
-count=0
-for key in g.keys():
-    for val in g[key]:
-        cluster_assign[Random_Edge_Placement(count)].append({key:val})
-        count+=1
 
 '''We need to now commence the random walk'''
 c={}
