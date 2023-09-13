44381c9bdae363ab7ce7db5ab206f304745bec57
jvap2 <jayvap6181@gmail.com>
2023-09-13 10:59:22 -0400

    init P matrix

--- CUDA/Homogenous/src/pagerank.cu
+++ CUDA/Homogenous/src/pagerank.cu
@@ -6,3 +6,10 @@
 it utilizes cublas and cusparse*/
 
 
+__global__ void Gen_P(float* weight_P,unsigned int* src, unsigned int* succ, unsigned int node_size){
+    unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if(x < node_size && y < node_size){
+        weight_P[y*node_size + x] = 1.0f/(src[x+1]-src[x]);
+    }
+}
